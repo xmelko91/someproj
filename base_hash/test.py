8fbee720ea35e250af79c7321fc925985ec35ec7
Sergei Khmelevskiy <xmelko91@gmail.com>
2018-12-05 08:58:15 +0300

    hash

--- base_hash/test.py
+++ base_hash/test.py
@@ -3,8 +3,8 @@ import requests
 path = '/home/serge/PycharmProjects/patt/base_hash/123.jpg'
 hh = '7206f89133daa9cdebdbf0ca4efdc480f235a682'
 
-#реквест на загрузку
-b = requests.get(('http://127.0.0.1:5000'), data='Upload '+path)
+#реквест на загрузку, получает хэш
+hh = requests.get(('http://127.0.0.1:5000'), data='Upload '+path)
 #реквест на выгрузку
 b = requests.get(('http://127.0.0.1:5000'), data='Download '+hh)
 #
